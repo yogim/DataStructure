8de663ef593b60f1c19d250114182f8f7b4d24a1
yogim <yogeshmhase89@gmail.com>
2018-02-04 16:51:07 -0800

    linked duplicate

--- src/com/ymhase/collection/CollectionDemo.java
+++ src/com/ymhase/collection/CollectionDemo.java
@@ -1,12 +1,12 @@
 package com.ymhase.collection;
 
 class CollectionDemo {
-	
-	public static void main(String args[]) {
-		String str = "abcd";
-		
-		int val = str.charAt(0)-'a';
-	
-	}
+
+    static int a = 2;
+
+    public static void main(String args[]) {
+	CollectionDemo cd = new CollectionDemo();
+
+    }
 
 }
--- src/com/ymhase/linkedlist/SinglyLL.java
+++ src/com/ymhase/linkedlist/SinglyLL.java
@@ -1,41 +1,65 @@
 package com.ymhase.linkedlist;
 
+import java.util.HashSet;
+
 public class SinglyLL {
 
-	private Node head= null;
+    private Node head = null;
 
-	public void createLinkedList(int data) {
-		head = new Node(data);
+    public void createLinkedList(int data) {
+	head = new Node(data);
+    }
+
+    public Node getAddressOfLastNode() {
+	Node iterator = head;
+	Node prevIterator = null;
+	while (iterator != null) {
+	    prevIterator = iterator;
+	    iterator = (Node) iterator.getNext();
 	}
+	return prevIterator;
+    }
 
-	public Node getAddressOfLastNode() {
-		Node iterator = head;
-		Node prevIterator = null;
-		while (iterator != null) {
-			System.out.println(iterator.getData());
-			prevIterator = iterator;
-			iterator = (Node) iterator.getNext();
-		}
-		return prevIterator;
+    public void addNode(int data) {
+	getAddressOfLastNode().setNext(new Node(data));
+    }
+
+    public void printLinkedList() {
+	Node iterator = head;
+	while (iterator != null) {
+	    System.out.println(iterator.getData());
+	    iterator = (Node) iterator.getNext();
 	}
+    }
 
-	public void addNode(int data) {
-		getAddressOfLastNode().setNext(new Node(data));
+    public void removeDuplicate() {
+	Node iterator = head;
+	Node previous = head;
+	HashSet<Integer> set = new HashSet<Integer>();
+	while (iterator != null) {
+
+	    if (!set.contains(iterator.getData())) {
+		set.add(iterator.getData());
+		
+	    } else {
+		previous.setNext(iterator.getNext());
+		
+	    }
+	    previous = iterator;
+	    iterator = (Node) iterator.getNext();
+	    
 	}
+    }
 
-	public void printLinkedList() {
-		Node iterator = head;
-		while (iterator != null) {
-			System.out.println(iterator.getData());
-			iterator = (Node) iterator.getNext();
-		}
-	}
+    public static void main(String args[]) {
+	SinglyLL ll = new SinglyLL();
+	ll.createLinkedList(1);
+	ll.addNode(2);
+	ll.addNode(2);
+	ll.addNode(3);
+	ll.addNode(3);
+	ll.removeDuplicate();
+	ll.printLinkedList();
 
-	public static void main(String args[]) {
-		SinglyLL ll = new SinglyLL();
-		ll.createLinkedList(1);
-		ll.addNode(2);
-		ll.printLinkedList(); /* print linkedlist */
-
-	}
+    }
 }
