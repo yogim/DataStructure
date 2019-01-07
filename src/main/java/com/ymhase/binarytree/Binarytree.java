3392e0a26860065daca3ca326c9cb2abc73319a1
Yogesh Mhase <yogeshmhase89@gmail.com>
2019-01-07 07:21:11 -0800

    linkedlist , binary tree, string problem solutionadd

--- src/main/java/com/ymhase/binarytree/Binarytree.java
+++ src/main/java/com/ymhase/binarytree/Binarytree.java
@@ -1,77 +1,86 @@
 package com.ymhase.binarytree;
 
-import java.util.Stack;
-
 public class Binarytree {
-    private BTNode root;
+	private BTNode root;
 
-    public Binarytree() {
-	this.root = null;
-	
-    }
-
-    public void addNode(int data) {
-	this.root= addNode(this.root, data);
-    }
-
-    public BTNode addNode(BTNode node, int data) {
-
-	if (node == null) {
-	    node = new BTNode(data);
-
-	} else {
-
-	    if (node.getData() <= data) {
-		node.left=addNode(node.left, data);
-		
-	    } else {
-		node.right=addNode(node.right, data);
-		
-	    }
+	public Binarytree() {
+		this.root = null;
 
 	}
-	
-	return node;
-    }
 
-    public void postTraversal() {
-	Stack<BTNode> stack1 = new Stack<>();
-	Stack<BTNode> stack2 = new Stack<>();
-
-	BTNode iterator = this.root;
-	stack1.push(iterator);
-	while (!stack1.isEmpty()) {
-
-	    stack2.push(stack1.pop());
-	    iterator = stack2.peek();
-	    if (iterator.left != null)
-		stack1.push(iterator.left);
-	    if (iterator.right != null)
-		stack1.push(iterator.right);
-	    if (!stack1.isEmpty())
-		iterator = stack1.peek();
+	public void addNode(int data) {
+		this.root = addNode(this.root, data);
 	}
 
-	while (!stack2.isEmpty()) {
-	    System.out.println(stack2.pop().data
+	public BTNode addNode(BTNode currentNode, int data) {
 
-	    );
+		if (currentNode == null) {
+			return new BTNode(data);
+
+		} else {
+
+			if (currentNode.getData() <= data) {
+				currentNode.left = addNode(currentNode.left, data);
+
+			} else {
+				currentNode.right = addNode(currentNode.right, data);
+
+			}
+
+		}
+
+		return currentNode;
 	}
 
-    }
+	public void inorderTrav() {
+		inorderTrav(this.root);
+	}
 
-    public static void main(String[] args) {
-	Binarytree bt = new Binarytree();
-	bt.addNode(5);
-	bt.addNode(2);
-	bt.addNode(7);
-	bt.addNode(1);
-	bt.addNode(6);
-	bt.addNode(5);
-	bt.addNode(11);
-	bt.addNode(9);
-	bt.addNode(4);
-bt.postTraversal();
-    }
+	public void preorderTrav() {
+
+		preorderTrav(this.root);
+
+	}
+
+	public void postTrav() {
+		postTrav(this.root);
+	}
+
+	public void inorderTrav(BTNode currentNode) { // left- root - right
+
+		if (currentNode != null) {
+			inorderTrav(currentNode.getLeft());
+			inorderTrav(currentNode.getRight());
+			System.out.println(currentNode.getData());
+		}
+
+	}
+
+	public void preorderTrav(BTNode currentNode) { // root left right
+
+		if (currentNode != null) {
+			System.out.println(currentNode.getData());
+			inorderTrav(currentNode.getLeft());
+			inorderTrav(currentNode.getRight());
+		}
+
+	}
+
+	private void postTrav(BTNode root) {
+
+	}
+
+	public static void main(String[] args) {
+		Binarytree bt = new Binarytree();
+		bt.addNode(5);
+		bt.addNode(2);
+		bt.addNode(7);
+		bt.addNode(1);
+		bt.addNode(6);
+		bt.addNode(11);
+		bt.addNode(9);
+		bt.addNode(4);
+		bt.inorderTrav(bt.root);
+	}
 
 }
--- src/main/java/com/ymhase/string/CheckRunner.java
+++ src/main/java/com/ymhase/string/CheckRunner.java
@@ -1,11 +1,31 @@
 package com.ymhase.string;
 
+import java.util.List;
+
 public class CheckRunner {
 
-    
-    public static void main(String[] args) {
-	
-	//AlgoFactory checker = UniqueStringChecker.cre
-	
-    }
+	public int removeObstacle(int numRows, int numColumns, List<List<Integer>> lot) {
+
+		int dist = removeObstacle(0, 0, numRows, numColumns, lot, 0);
+		return dist;
+	}
+
+	private int removeObstacle(int i, int j, int numRows, int numColumns, List<List<Integer>> lot, int dist) {
+		if (i < 0 && i >= numRows && j < 0 && j >= numColumns && lot.get(i).get(j) == 0) {
+			return 0;
+		}
+		if (lot.get(i).get(j) == 9) {
+			return dist;
+		}
+		dist = removeObstacle(i + 1, j, numRows, numColumns, lot, dist);
+		dist = removeObstacle(i - 1, j, numRows, numColumns, lot, dist);
+		dist = removeObstacle(i, j + 1, numRows, numColumns, lot, dist);
+		dist = removeObstacle(i, j - 1, numRows, numColumns, lot, dist);
+
+		return dist + 1;
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
